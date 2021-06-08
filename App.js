01b849e0e7c47005991bace53d9535cc7cecaa42
Asraful Islam <iasraful321@gmail.com>
2021-06-08 20:01:38 +0600

    Added Header and Imported components in App Component

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import BookList from './components/BooksList';
+import Header from './components/Header';
 
 export default function App() {
-  return <>HEY</>;
+  return (
+    <View style={styles.container}>
+      <Header />
+      <BookList />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
